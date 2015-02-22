81b629393938c63d8eed0e936206ccee6873b303
Kyle Mitchell <kyle@kemitchell.com>
2015-02-22 07:51:07 +0000

    seed with prior input and output

--- source/index.js
+++ source/index.js
@@ -1,28 +1,48 @@
+var Immutable = require('immutable');
 var hash = require('commonform-hash')
 var isSubForm = require('commonform-predicate').subForm
 
-module.exports = function merkleize(form) {
-  return form.withMutations(function(mutableForm) {
+module.exports = function merkleize(form, prior, priorResult) {
+  if (
+    prior !== false &&
+    priorResult !== false &&
+    Immutable.is(form, prior)) {
+      return priorResult}
 
-    var merkleizedContent =
-      form
-        .get('content')
-        .map(function(element) {
-          return isSubForm(element) ?
-            element.update('form', merkleize) :
-            element})
+  else {
+    return form.withMutations(function(mutableForm) {
 
-    var normalized =
-      form.set(
-        'content',
-        merkleizedContent
-          .map(function(element) {
+      var merkleizedContent =
+        form
+          .get('content')
+          .map(function(element, index) {
             return isSubForm(element) ?
-              element.set('form', element.getIn(['form', 'digest'])) :
-              element}))
+              // A sub-form that needs a hash
+              element.update('form', function(subForm) {
+                if (prior && priorResult) {
+                  var keyArray = ['content', index, 'form']
+                  return merkleize(
+                    subForm,
+                    prior.getIn(keyArray, false),
+                    priorResult.getIn(keyArray, false))}
+                else {
+                  return merkleize(subForm, false, false)
+                }
+              }) :
+              // Not a sub-form that needs a hash
+              element})
 
-    mutableForm
-      .set('content', merkleizedContent)
-      .set('digest', hash(normalized))})}
+      var normalized =
+        form.set(
+          'content',
+          merkleizedContent
+            .map(function(element) {
+              return isSubForm(element) ?
+                element.set('form', element.getIn(['form', 'digest'])) :
+                element}))
+
+      mutableForm
+        .set('content', merkleizedContent)
+        .set('digest', hash(normalized))})}}
 
 module.exports.version = '0.1.0';
--- test/merkleize.test.js
+++ test/merkleize.test.js
@@ -42,4 +42,71 @@ describe('merkleize', function() {
                 '78a186f26647f2aaa7249298696e6f22'}}],
         digest:
           '84fd358ac5b0109eaca10d1224e1c52a' +
-          'a1d2de4f0b6a741303c05b318c55b326'})})})
+          'a1d2de4f0b6a741303c05b318c55b326'})})
+
+  it('accepts an a prior input-output pair to start from', function() {
+    expect(
+      merkleize(
+        // Updated form with one different sub-form
+        Immutable.fromJS({
+          content: [
+            {form: {content: ['A']}},
+            {form: {content: ['B']}}]}),
+        // Prior form with the same sub-form twice
+        Immutable.fromJS({
+          content: [
+            {form: {content: ['A']}},
+            {form: {content: ['A']}}]}),
+        // merkleize() result for a different form, with the same
+        // sub-form appearing twice
+        Immutable.fromJS({
+          // This parent digest should not be reused, since a sub-form
+          // has changed.
+          digest:
+            '9b95d3e47c2233b2c7c4fce356ff01ab' +
+            'f1eb85cc1fffc2710f52ea26dbfcd20b',
+          content: [
+            {
+              form: {
+                // This digest should be reused, since the same sub-form
+                // appears in the same position within its parent's
+                // content list.
+                digest:
+                  // The actual digest for this form would be:
+                  //
+                  // 'eb94d16f10023fe29cb75d02a60eb531' +
+                  // 'ffedcc7bdf7cc9aba8c25c962116b1f9',
+                  //
+                  // Here, we use a dummy value, so our test can ensure
+                  // that it is being reused.
+                  'previously computed digest',
+                content: ['A']}},
+            {
+              form: {
+                // This digest should not be reused.
+                digest:
+                  'eb94d16f10023fe29cb75d02a60eb531' +
+                  'ffedcc7bdf7cc9aba8c25c962116b1f9',
+                content: ['A']}}]})).toJS())
+      .to.eql({
+        content: [
+          {
+            form: {
+              content: ['A'],
+              digest: 'previously computed digest'}},
+          {
+            form: {
+              content: ['B'],
+              digest:
+                '5e5d60591967ee74ef2d324abc4b4485'+
+                '78a186f26647f2aaa7249298696e6f22'}}],
+        digest:
+          // The actual digest would be:
+          //
+          // '84fd358ac5b0109eaca10d1224e1c52a' +
+          // 'a1d2de4f0b6a741303c05b318c55b326'
+          //
+          // Since we're using a fake digest to ensure its value is
+          // reused, the digest we actually expect is:
+          '66ece68994b874e627c34bd33333d00cf' +
+          '6ea4a651ab2a4fe471fca2ed84bdc27'})})})
