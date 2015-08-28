ea0f88acbcdf4d63f7126ade92467a9058ee908d
Kyle Mitchell <kyle@kemitchell.com>
2015-08-27 23:51:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+```javascript
+var merkleize = require('commonform-merkleize')
+var assert = require('assert')
+
+assert.deepEqual(
+  merkleize({ content: [ 'This is a test' ] }),
+  { digest: (
+    'a4b1cdbb7425a4a195f3dfe748fe7ae7' +
+    'da59d6ddb83ae7a46ffde9d8a7e86450' ) })
+
+assert.deepEqual(
+  merkleize({ content: [ { form: { content: [ 'This is a test' ] } } ] }),
+  { digest: (
+      '7783a0c438b8ad057806890d040d06b8' +
+      '69bdcd04bef4ea01cd2e35257631b823' ),
+    content: {
+      0: { digest: (
+        'a4b1cdbb7425a4a195f3dfe748fe7ae7' +
+        'da59d6ddb83ae7a46ffde9d8a7e86450' ) } } })
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+var normalize = require('commonform-normalize')
+
+function isChild(object) {
+  return (
+    typeof object === 'object' &&
+    'digest' in object ) }
+
+function recurse(normalized, digest) {
+  var form = normalized[digest]
+  if (form.content.some(isChild)) {
+    return {
+      digest: digest,
+      content: form.content
+        .reduce(
+          function(content, element, index) {
+            if (isChild(element)) {
+              content[index] = recurse(normalized, element.digest) }
+            return content },
+          { }) } }
+  else {
+    return { digest: digest } } }
+
+function merkleize(form) {
+  var normalized = normalize(form)
+  return recurse(normalized, normalized.root) }
+
+module.exports = merkleize
