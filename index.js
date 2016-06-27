27f7959848ac757cbb495d200260d546d9b1ad39
Kyle E. Mitchell <kyle@kemitchell.com>
2016-06-27 16:18:09 -0700

    Use standard style

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 var normalize = require('commonform-normalize')
 
-function isChild(object) {
-  return (
-    typeof object === 'object' &&
-    'digest' in object ) }
+function isChild (object) {
+  return typeof object === 'object' && 'digest' in object
+}
 
-function recurse(normalized, digest) {
+function recurse (normalized, digest) {
   var form = normalized[digest]
   if (form.content.some(isChild)) {
     return {
       digest: digest,
-      content: form.content
-        .reduce(
-          function(content, element, index) {
-            if (isChild(element)) {
-              content[index] = recurse(normalized, element.digest) }
-            return content },
-          { }) } }
-  else {
-    return { digest: digest } } }
+      content: form.content.reduce(function (content, element, index) {
+        if (isChild(element)) content[index] = recurse(normalized, element.digest)
+        return content
+      }, {})
+    }
+  } else return {digest: digest}
+}
 
-function merkleize(form) {
+function merkleize (form) {
   var normalized = normalize(form)
-  return recurse(normalized, normalized.root) }
+  return recurse(normalized, normalized.root)
+}
 
 module.exports = merkleize
